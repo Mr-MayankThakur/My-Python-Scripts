d71841129427b2a968112ec893dcdb3a6bdeec34
mayankthakur <mackv210w@gmail.com>
2020-08-15 00:58:19 +0530

    fix running while importing

--- scripts/Project_Euler/problem_1.py
+++ scripts/Project_Euler/problem_1.py
@@ -1,11 +1,18 @@
 # sum of multiples of 3 and 5 below 1000
-N = 1000
-multiples = set()
 
-for x in range(0, N):
-    if x % 3 == 0:
-        multiples.add(x)
-    if x % 5 == 0:
-        multiples.add(x)
-print(multiples)
-print(f"Sum of multiples of 3 or 5 below {N} is {sum(multiples)}")
+def get_multiples(N):
+    multiples = set()
+
+    for x in range(0, N):
+        if x % 3 == 0:
+            multiples.add(x)
+        if x % 5 == 0:
+            multiples.add(x)
+
+    return multiples
+
+if __name__ == '__main__':
+    max_num = 1000
+    multiples = get_multiples(max_num)
+    print(multiples)
+    print(f"Sum of multiples of 3 or 5 below {max_num} is {sum(multiples)}")
